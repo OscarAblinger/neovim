d8f6f07c958de638c9816b0a5e9f4b3e16567607
Oscar Ablinger <dev.ablinger@gmail.com>
2024-03-17 23:55:11 +0100

    more treesitter

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,2 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- requires nvim-treesitter
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,30 @@
 return {
 	{
+		-- also see entry in options.lua for folding
 		'nvim-treesitter/nvim-treesitter',
 		build = ':TSUpdate',
 		config = function()
-			local configs = require('nvim-treesitter.configs')
-
-			configs.setup({
-				ensure_installed = 'all'
+			require('nvim-treesitter.configs').setup({
+				ensure_installed = 'all',
+				highlight = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = 'gvn',
+                                                node_incremental = '+',
+                                                node_decremental = '-',
+                                                scope_incremental = 'g+',
+					},
+				},
 			})
 		end,
 	},
+	{	-- todo: doesn't work :(
+		'ziontee113/syntax-tree-surfer',
+		dependencies = { 'nvim-treesitter/nvim-treesitter' },
+		keys = {
+			{ 'vx', '<cmd>STSSelectMasterNode<cr>', 'n', desc = 'Select Treesitter master node' },
+			{ 'vn', '<cmd>STSSelectCurrentNode<cr>', 'n', desc = 'Select Treesitter current node' },
+		},
+	}
 }
